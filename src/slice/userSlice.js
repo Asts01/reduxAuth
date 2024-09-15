a2d7e450d236eb151de0a7da837f94ec47f1ffa1
Asts01 <aasthajain22092003@gmail.com>
2024-09-15 19:42:46 +0530

    commit-1

--- src/slice/userSlice.js
+++ src/slice/userSlice.js
@@ -1,5 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { act } from 'react';
+// import { act } from 'react';
 
 export const UserSlice = createSlice({
   name: 'user',
@@ -10,24 +10,24 @@ export const UserSlice = createSlice({
     isAuthenticated: false,  // Authentication status
   },
   reducers: {
-    // Clears user data and resets authentication state
+    // to Clear all the user data and resets authentication state
     emptyOutState: (state) => {
       state.email = '';
       state.name = '';
       state.isAuthenticated = false;
     },
-    // Updates user data upon successful login/signup
+    // to update user data upon successful login/signup
     updateUser: (state, action) => {
       state.name = action.payload.name;
       state.email = action.payload.email;
     },
-    // Adds a new user to the list of all users
+    // to add a new user to the list of all users
     addUser: (state, action) => {
       state.allUsers.push(action.payload);
       console.log(action.payload);
       console.log(state.allUsers);
     },
-    // Checks if a user exists and can be logged in
+    // to check whether the desired user exists and can be logged in
     canLoginUser: (state, action) => {
       console.log("action.payload",action.payload);
       const user = state.allUsers.find(
--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
-// import TaskSlice from '../slices/taskSlice'
 import UserSlice from '../slice/userSlice';
 
 const store =  configureStore({
   reducer: {
-    // task:TaskSlice,
     user:UserSlice,
   },
 })
